86c044f91a5742a828f3267b9129f525e774737a
Vanderlei Kempf <dev.vjk@gmail.com>
2022-07-11 22:04:19 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificationButton from "./components/NotificationButon"
+
 function App() {
   return (
     <>
       <h1>Olá Mundo!</h1>
       <h1>Hello World!</h1>
+      <NotificationButton />
+      <NotificationButton />
     </>
   )
 }
